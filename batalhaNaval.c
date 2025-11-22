12952dee419e22a7e179dd1289dd665ac7ede641
Rennan Marques <rennan.marques.2222@gmail.com>
2025-11-22 00:36:39 +0000

    Projeto iniciante batalha naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,37 +4,54 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+
+// desafio no nivel novato.
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    // Definindo o tamanho do tabuleiro
+    int linhas = 10, colunas = 10;
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Coordenadas dos navios
+    int navioVertical[3][2];  
+    int navioHorizontal[3][2];
+
+    // Navio vertical começando em 2,4
+    int xInicialV = 2;
+    int yInicialV = 4;
+
+    for (int i = 0; i < 3; i++) {
+        navioVertical[i][0] = xInicialV + i; // X aumenta verticalmente
+        navioVertical[i][1] = yInicialV;
+    }
+
+    // Navio horizontal começando em (6, 1)
+    int xInicialH = 6;
+    int yInicialH = 1;
+
+    for (int i = 0; i < 3; i++) {
+        navioHorizontal[i][0] = xInicialH;
+        navioHorizontal[i][1] = yInicialH + i; // Y aumenta horizontalmente
+    }
+
+    //Print das coordenadas
+    
+    printf("=== Coordenadas do Navio Vertical ===\n");
+    for (int i = 0; i < 3; i++) {
+        printf("Parte %d -> (X: %d, Y: %d)\n", 
+               i + 1, navioVertical[i][0], navioVertical[i][1]);
+    }
+
+    printf("\n=== Coordenadas do Navio Horizontal ===\n");
+    for (int i = 0; i < 3; i++) {
+        printf("Parte %d -> (X: %d, Y: %d)\n", 
+               i + 1, navioHorizontal[i][0], navioHorizontal[i][1]);
+    }
 
     return 0;
 }
+    
+
